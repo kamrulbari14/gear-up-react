2654ef7c7f728fe44c190ed7c54c867378b42ae2
Kamrul Bari <69103450+kamrulbari14@users.noreply.github.com>
2023-03-04 17:26:01 +0600

    initiate react app

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>Hello World!!!!!</b>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/md-kamrul-bari/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          About Me
         </a>
       </header>
     </div>
